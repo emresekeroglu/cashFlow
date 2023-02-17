f3aa07d6141c5ef4b9e2f8be4d6f03b1f2f10add
Emre Şekeroğlu <emresekeroglu08@gmail.com>
2023-02-18 00:11:00 +0300

    Add New Models... - Add Customer Model - Add AccountAction Model - Add Admin Panel Searchable Dropdown List - Add Foreign Key and Setup Config

--- core/cashFlow/admin.py
+++ core/cashFlow/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from .models import AccountBox
+from .models import AccountBox, Customer, AccountAction
 
 class AccountBoxAdmin(admin.ModelAdmin):
+  search_fields = ['accountName']
   list_display = ('accountName','accountAmount')
+  
+class CustomerAdmin(admin.ModelAdmin):
+  search_fields = ['CustomerTitle']
+  list_display = ('CustomerTitle','CustomerCode','CustomerPhone')
+  
+class AccountActionAdmin(admin.ModelAdmin):
+  autocomplete_fields = ['accountName','CustomerTitle']
+  list_display = ('CustomerTitle','accountName','in_out_flow','amount')
   
   
 admin.site.register(AccountBox,AccountBoxAdmin)
+admin.site.register(Customer,CustomerAdmin)
+admin.site.register(AccountAction,AccountActionAdmin)
--- core/cashFlow/models.py
+++ core/cashFlow/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 # Create your models here.
 class AccountBox(models.Model):
-    accountName = models.CharField("Hesap Adı", max_length=250, blank=True, null=True)
-    accountAmount = models.DecimalField("Toplam Tutar", max_digits=10, decimal_places=2)
-    accountImge = models.ImageField("Banka Görseli", null=True, blank=True, upload_to="images/")
+    accountName = models.CharField(
+        "Hesap Adı", max_length=250, blank=True, null=True, default="empty value"
+    )
+    accountAmount = models.DecimalField("Toplam Tutar", max_digits=20, decimal_places=2)
+    accountImge = models.ImageField(
+        "Banka Görseli", null=True, blank=True, upload_to="images/"
+    )
     accountStatus = models.BooleanField("Aktif Mi?", null=False, default=True)
 
     class Meta:
@@ -16,3 +20,57 @@ class AccountBox(models.Model):
 
     def get_absolute_url(self):
         return reverse("_detail", kwargs={"pk": self.pk})
+
+
+class Customer(models.Model):
+    CustomerTitle = models.CharField(
+        "Firma Ünvanı", max_length=255, null=False, blank=False, default="empty value"
+    )
+    CustomerCode = models.CharField(
+        "Cari Kodu", max_length=255, null=False, blank=False
+    )
+    CustomerPhone = models.CharField("Telefon", max_length=12, null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Firma"
+        verbose_name_plural = "Firmalar"
+
+    def __str__(self):
+        return f"{self.CustomerTitle}"
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
+
+
+class AccountAction(models.Model):
+    CustomerTitle = models.ForeignKey(Customer, verbose_name = 'Firma Adı', on_delete=models.CASCADE)
+    accountName = models.ForeignKey(AccountBox, verbose_name = 'Hesap Adı', on_delete=models.CASCADE)
+    InFlow = "GİRİŞ"
+    OutFlow = "ÇIKIŞ"
+    IN_OUT_FLOW_CHOICES = [
+        (InFlow, "Giriş"),
+        (OutFlow, "Çıkış"),
+    ]
+    in_out_flow = models.CharField(
+        "Giriş/Çıkış", max_length=5, choices=IN_OUT_FLOW_CHOICES, default=InFlow
+    )
+    amount = models.DecimalField("Tutar", max_digits=20, decimal_places=2)
+
+    class Meta:
+        verbose_name = "Hesap Hareketi"
+        verbose_name_plural = "Hesap Hareketleri"
+
+    def __str__(self):
+        return f"{self.accountName}"
+
+'''
+    def get_accounts(self):
+        return "\n".join([a.accountName for a in self.accountName.all()])
+
+    get_accounts.short_description = "Hesap Adı"
+
+    def get_customers(self):
+        return "\n".join([c.CustomerTitle for c in self.CustomerTitle.all()])
+
+    get_customers.short_description = "Firma Adı"
+'''
